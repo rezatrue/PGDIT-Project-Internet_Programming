6bc86057464ebc518a80b13dd23a604164bfaa5a
rezatrue <rezatrue@gmail.com>
2018-08-21 11:48:20 +0600

    data insert & fetch done

--- contact.php
+++ contact.php
@@ -12,7 +12,53 @@ include_once("header.php");
 	include_once("sidebar_content.php");
 
 	?>
-	
+
+		<?php
+		
+		if(isset($_REQUEST["username"]) && isset($_REQUEST["email"]) && isset($_REQUEST["subject"]) && isset($_REQUEST["msg"]))
+		{
+			$nam = $_REQUEST["username"];
+			$ema = $_REQUEST["email"];
+			$sub = $_REQUEST["subject"];
+			$msg = $_REQUEST["msg"];
+			//echo "First Name:" . $fn . "<br>";
+			//echo "Last Name:" . $ln ;
+			
+			$cn = mysql_connect ("localhost","root","");
+			if ( !$cn)
+			{
+				die("could not connect".mysql_error());
+			}
+			else
+			{
+				echo "connected to the server successfully";
+			}
+			
+			mysql_select_db("pgdit1815",$cn);
+			
+			$q ="INSERT INTO contact_list (name, email, subject, message)
+				VALUES ('". $nam ."', '". $ema ."', '". $sub ."', '". $msg ."')";
+				
+			
+			//echo $q;
+			
+			$mq = mysql_query($q,$cn);
+			if (!$mq)
+			{
+				echo "Could not inserted data".mysql_error();
+			}
+			else
+			{
+				echo "data inserted successfuly";
+			}
+			mysql_close($cn);
+
+			
+		}
+		
+		
+		?>
+
     <?php
 
 	include_once("contact_form.php");
--- contact_list.php
+++ contact_list.php
@@ -4,6 +4,7 @@ include_once("header.php");
 
 ?>
 
+
     <div id="content_header"></div>
     <div id="site_content">
     
@@ -12,13 +13,73 @@ include_once("header.php");
 	include_once("sidebar_content.php");
 
 	?>
+
+	<?php
 	
-    <?php
+	
+	if(isset($_REQUEST["username"]) && isset($_REQUEST["pass"]))
+	{
+		if($_REQUEST["username"] == "admin" && $_REQUEST["pass"] == "123")
+		{
+			$_SESSION["user"] = "admin";
+			//header('Location: userlist.php');
+			
+			echo "<h2 style="."text-align:center"." >Welcome Admin here's contact list : ".
+			"<span style="."text-align:right"." ><a href="."signout.php"." > Sign out </a> </span></h2>";
 
-	include_once("content_main.php");
+			echo "<h1 style="."text-align:center"." > Contact List in here</h1>";
+			
+			
+			$cn = mysql_connect ("localhost","root","");
+			if ( !$cn)
+			{
+				die("could not connect".mysql_error());
+			}
+			else
+			{
+				//echo "connected to the server successfully";
+			}
+			
+			mysql_select_db("pgdit1815",$cn);
+			
+			$q ="SELECT * FROM contact_list";
+			//* = All
+			$mq = mysql_query($q,$cn);
+			echo "<div text-align= 'center'>"; 
+			echo "<table text-align= 'center' border='1'><tr><th>SL. NO.</th><th>Name</th><th>Email</th><th>Subject</th><th>Message</th></tr>";
+			while($row = mysql_fetch_array($mq))
+			{
+				echo "<tr><td>";
+				echo $row['slno'] . "</td><td> " . $row['name']. "</td><td> " . $row['email']. "</td><td> " . $row['subject']. "</td><td> " . $row['message'];
+				echo "</td></tr>";
+			}
+			echo "</table>";
+			echo "</div>";
+			mysql_close($cn);
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		else
+		{
+			echo "<h6 style="."text-align:center;color:red"." >wrong user name or password</h6>";
+			include_once("admin_form.php");
+		}
+		
+	}
+	else
+		{
+			include_once("admin_form.php");
+		}
+		
+	?>
+	
 
-	?>  
-	  
     </div>
 
 <?php
